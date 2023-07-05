4c6f929228431a6e8d003c6bbcac4500b90f8a5e
loong917 <loong.me@outlook.com>
2023-07-05 16:21:28 +0800

    doublylinkedlist

---
+++ doublylinkedlist.go
@@ -0,0 +1,175 @@
+package algorithm
+
+import "fmt"
+
+// 双向链表
+type DoublyLinkedList struct {
+	head   *Node
+	tail   *Node
+	length uint
+}
+
+type Node struct {
+	data     interface{}
+	previous *Node
+	next     *Node
+}
+
+func NewDoublyLinkedList() *DoublyLinkedList {
+	return &DoublyLinkedList{
+		length: 0,
+	}
+}
+
+// 新增头节点
+func (list *DoublyLinkedList) InsertToHead(v interface{}) error {
+	newNode := &Node{
+		data: v,
+	}
+	// 头节点
+	head := list.head
+	if head == nil {
+		// 空链表
+		list.head = newNode
+		list.tail = newNode
+	} else {
+		// 非空链表
+		newNode.next = head
+		head.previous = newNode
+		list.head = newNode
+	}
+	list.length++
+	return nil
+}
+
+// 删除头节点
+func (list *DoublyLinkedList) RemoveHead() error {
+	if list.head == nil {
+		return ErrRemoveFromEmptyList
+	}
+	// 新头节点
+	next := list.head.next
+	next.previous = nil
+	list.head = next
+	list.length--
+	return nil
+}
+
+// 在某个节点后面插入节点
+func (list *DoublyLinkedList) InsertAfter(node *Node, v interface{}) error {
+	if node == nil {
+		return ErrEmptyNode
+	}
+	newNode := &Node{
+		data: v,
+	}
+	tail := list.tail
+	if node == tail {
+		// 在尾节点后插入
+		newNode.previous = tail
+		tail.next = newNode
+		list.tail = newNode
+	} else {
+		// 不是尾节点后插入
+		newNode.previous = node
+		newNode.next = node.next
+		node.next = newNode
+	}
+	list.length++
+	return nil
+}
+
+// 在某个节点前面插入节点
+func (list *DoublyLinkedList) InsertBefore(node *Node, v interface{}) error {
+	if node == nil {
+		return ErrEmptyNode
+	}
+	// 空链表或者在头节点前插入
+	if list.head == nil || node == list.head {
+		return list.InsertToHead(v)
+	}
+	previous := node.previous
+	newNode := &Node{
+		data:     v,
+		next:     node,
+		previous: previous,
+	}
+	// 前一节点的next
+	previous.next = newNode
+	// 当前节点的previous
+	node.previous = newNode
+	list.length++
+	return nil
+}
+
+// 新增尾节点
+func (list *DoublyLinkedList) InsertToTail(v interface{}) error {
+	if list.head == nil {
+		return list.InsertToHead(v)
+	}
+	tail := list.tail
+	newNode := &Node{
+		data:     v,
+		previous: tail,
+	}
+	tail.next = newNode
+	list.tail = newNode
+	list.length++
+	return nil
+}
+
+// 删除尾节点
+func (list *DoublyLinkedList) RemoveTail() error {
+	if list.head == nil {
+		return ErrRemoveFromEmptyList
+	}
+	tail := list.tail
+	// 新的尾节点
+	current := tail.previous
+	current.next = nil
+	list.tail = current
+	list.length--
+	return nil
+}
+
+// 删除节点
+func (list *DoublyLinkedList) RemoveNode(node *Node) error {
+	if node == nil {
+		return ErrEmptyNode
+	}
+	if list.head == nil {
+		return ErrRemoveFromEmptyList
+	}
+	// 前一节点的next
+	current := node.previous
+	current.next = node.next
+	// 后一节点的previous
+	next := node.next
+	next.previous = current
+	list.length--
+	return nil
+}
+
+// 反转链表
+func (list *DoublyLinkedList) Reverse() {
+	if list.head == nil {
+		return
+	}
+}
+
+// 打印链表
+func (list *DoublyLinkedList) Print() string {
+	if list.head == nil {
+		return ""
+	}
+	current := list.head
+	var message string
+	for current != nil {
+		message += fmt.Sprintf("%+v", current.data)
+		current = current.next
+		if current != nil {
+			message += "->"
+		}
+	}
+	return message
+}
---
+++ doublylinkedlist_test.go
@@ -0,0 +1,283 @@
+package algorithm
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestEmptyDoublyInsertToHead(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var (
+		in       = "1"
+		expected = "1"
+	)
+	err := list.InsertToHead(in)
+	if err != nil {
+		t.Errorf("insert to head：%s", err.Error())
+		return
+	}
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("insert '%s' to head is '%s'", in, expected)
+	} else {
+		t.Errorf("insert '%s' to head is '%s'; expected '%s'", in, actual, expected)
+	}
+}
+
+func TestDoublyInsertToHead(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var err error
+	err = list.InsertToHead("1")
+	if err != nil {
+		t.Errorf("insert to head：%s", err.Error())
+		return
+	}
+	t.Logf("before insert to head is '%s' ", list.Print())
+	err = list.InsertToHead("2")
+	if err != nil {
+		t.Errorf("insert to head： %s", err.Error())
+		return
+	}
+	var expected string = "2->1"
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("after insert to head is '%s' ", expected)
+	} else {
+		t.Errorf("insert to head is '%s'; expected '%s'", actual, expected)
+	}
+}
+
+func TestEmptyDoublyRemoveHead(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var expected error = ErrRemoveFromEmptyList
+	err := list.RemoveHead()
+	if err == nil {
+		t.Errorf("remove head is %v'; expected %v", err, expected)
+		return
+	}
+	if errors.Is(err, expected) {
+		t.Logf("remove head：%v", err)
+	} else {
+		t.Errorf("remove head：%v", err)
+	}
+}
+
+func TestDoublyRemoveHead(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var err error
+	err = list.InsertToHead("1")
+	if err != nil {
+		t.Errorf("remove head：%s", err.Error())
+		return
+	}
+	err = list.InsertToHead("2")
+	if err != nil {
+		t.Errorf("remove head：%s", err.Error())
+		return
+	}
+	t.Logf("before remove head is '%s'", list.Print())
+	err = list.RemoveHead()
+	if err != nil {
+		t.Errorf("remove head：%s", err.Error())
+		return
+	}
+	var expected string = "1"
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("after remove head is '%s'", expected)
+	} else {
+		t.Errorf("remove head is '%s'; expected '%s'", actual, expected)
+	}
+}
+
+func TestDoublyInsertToTail(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var err error
+	err = list.InsertToTail("1")
+	if err != nil {
+		t.Errorf("insert to tail：%s", err.Error())
+		return
+	}
+	t.Logf("before insert to tail is '%s'", list.Print())
+	err = list.InsertToTail("2")
+	if err != nil {
+		t.Errorf("insert to tail：%s", err.Error())
+		return
+	}
+	var expected string = "1->2"
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("after insert to tail is '%s'", expected)
+	} else {
+		t.Errorf("insert to tail is '%s'; expected '%s'", actual, expected)
+	}
+}
+
+func TestDoublyRemoveTail(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var err error
+	err = list.InsertToTail("1")
+	if err != nil {
+		t.Errorf("remove tail：%s", err.Error())
+		return
+	}
+	err = list.InsertToTail("2")
+	if err != nil {
+		t.Errorf("remove tail：%s", err.Error())
+		return
+	}
+	t.Logf("before remove tail is '%s'", list.Print())
+	err = list.RemoveTail()
+	if err != nil {
+		t.Errorf("remove tail：%s", err.Error())
+		return
+	}
+	var expected string = "1"
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("after remove tail is '%s'", expected)
+	} else {
+		t.Errorf("remove tail is '%s'; expected '%s'", actual, expected)
+	}
+}
+
+func TestDoublyRemoveNode(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var err error
+	err = list.InsertToTail("1")
+	if err != nil {
+		t.Errorf("remove node：%s", err.Error())
+		return
+	}
+	err = list.InsertToTail("2")
+	if err != nil {
+		t.Errorf("remove node：%s", err.Error())
+		return
+	}
+	err = list.InsertToTail("4")
+	if err != nil {
+		t.Errorf("remove node：%s", err.Error())
+		return
+	}
+	t.Logf("before remove node is '%s'", list.Print())
+	var (
+		in       = "2"
+		expected = "1->4"
+	)
+	current := list.head
+	var length uint = list.length
+	var i uint = 0
+	var node *Node
+	for ; i < length; i++ {
+		if current.data == in {
+			node = current
+			break
+		}
+		current = current.next
+	}
+	err = list.RemoveNode(node)
+	if err != nil {
+		t.Errorf("remove node：%s", err.Error())
+		return
+	}
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("after remove node is '%s'", expected)
+	} else {
+		t.Errorf("remove node is '%s'; expected '%s'", actual, expected)
+	}
+}
+
+func TestDoublyInsertAfter(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var err error
+	err = list.InsertToTail("1")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	err = list.InsertToTail("2")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	err = list.InsertToTail("3")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	t.Logf("before insert node is '%s'", list.Print())
+	var (
+		in       = "3"
+		expected = "1->2->3->4"
+	)
+	current := list.head
+	var length uint = list.length
+	var i uint = 0
+	var node *Node
+	for ; i < length; i++ {
+		if current.data == in {
+			node = current
+			break
+		}
+		current = current.next
+	}
+	err = list.InsertAfter(node, "4")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("after insert node is '%s'", expected)
+	} else {
+		t.Errorf("insert node is '%s'; expected '%s'", actual, expected)
+	}
+}
+
+func TestDoublyInsertBefore(t *testing.T) {
+	list := NewDoublyLinkedList()
+	var err error
+	err = list.InsertToTail("1")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	err = list.InsertToTail("2")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	err = list.InsertToTail("3")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	t.Logf("before insert node is '%s'", list.Print())
+	var (
+		in       = "3"
+		expected = "1->2->4->3"
+	)
+	current := list.head
+	var length uint = list.length
+	var i uint = 0
+	var node *Node
+	for ; i < length; i++ {
+		if current.data == in {
+			node = current
+			break
+		}
+		current = current.next
+	}
+	err = list.InsertBefore(node, "4")
+	if err != nil {
+		t.Errorf("insert node：%s", err.Error())
+		return
+	}
+	actual := list.Print()
+	if actual == expected {
+		t.Logf("after insert node is '%s'", expected)
+	} else {
+		t.Errorf("insert node is '%s'; expected '%s'", actual, expected)
+	}
+}
