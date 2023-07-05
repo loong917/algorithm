2e3c8d0fa8a7364e56c51cf3c3fd28b31f969dcc
loong917 <loong.me@outlook.com>
2023-07-05 12:15:57 +0800

    string reverse

--- reverse_test.go
+++ reverse_test.go
@@ -2,12 +2,12 @@ package algorithm
 
 import "testing"
 
-func TestReverse(t *testing.T) {
+func TestReverseString(t *testing.T) {
 	var (
 		in       = "功成不必在我"
 		expected = "我在必不成功"
 	)
-	actual := Reverse(in)
+	actual := ReverseString(in)
 	if actual == expected {
 		t.Logf("reverse '%s' is '%s'", in, expected)
 	} else {
