7fc8d9897e0429aa3750f2af17c112f33b8bf82f
loong917 <loong.me@outlook.com>
2023-07-31 09:24:55 +0800

    add 二分查找

--- binarysearch_test.go
+++ binarysearch_test.go
@@ -1,15 +1,17 @@
 package algorithm
 
 import (
+	"math"
 	"testing"
 )
 
 func TestBinarySearch(t *testing.T) {
+	math.Sqrt(2)
 	var (
-		array    = []int{1, 2, 3, 4, 5, 6}
+		array    = []int{1, 3, 5, 7, 10, 14}
 		expected = 4
 	)
-	var input int = 5
+	var input int = 10
 	actual := BinarySearch(array, input)
 	if actual == expected {
 		t.Logf("二分查找：the index of %d in %v is %d", input, array, actual)
