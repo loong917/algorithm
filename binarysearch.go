32deac53fb8378aaeda05d322a3b3d4d00c3cd55
loong917 <loong.me@outlook.com>
2023-08-01 08:46:52 +0800

    add 二分查找有序循环数组

--- binarysearch.go
+++ binarysearch.go
@@ -29,3 +29,139 @@ func binarySearchRecursive(array []int, value int, low int, high int) int {
 		return binarySearchRecursive(array, value, mid+1, high)
 	}
 }
+
+// 二分查找循环有序数组
+// 循环有序数组从任意位置分成两半后，至少有一半是有序的。
+func BinarySearchForRotateArray(array []int, value int) int {
+	var length int = len(array)
+	if length == 0 {
+		return -1
+	}
+	var low int = 0
+	var high int = length - 1
+	for low <= high {
+		mid := low + ((high - low) >> 1)
+		if array[mid] == value {
+			return mid
+		} else if array[mid] > array[low] {
+			// 左半段有序
+			if array[low] <= value && value < array[mid] {
+				high = mid - 1
+			} else {
+				low = mid + 1
+			}
+		} else {
+			// 右半段有序
+			if array[mid] < value && value <= array[high] {
+				low = mid + 1
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+// 查找第一个值等于给定值的元素
+func BinarySearchFirst(array []int, value int) int {
+	var length int = len(array)
+	if length == 0 {
+		return -1
+	}
+	var low int = 0
+	var high int = length - 1
+	for low <= high {
+		var mid int = low + ((high - low) >> 1)
+		if array[mid] > value {
+			high = mid - 1
+		} else if array[mid] < value {
+			low = mid + 1
+		} else {
+			if mid == 0 {
+				return mid
+			} else if array[mid-1] != value {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+// 查找最后一个值等于给定值的元素
+func BinarySearchLast(array []int, value int) int {
+	var length int = len(array)
+	if length == 0 {
+		return -1
+	}
+	var low int = 0
+	var high int = length - 1
+	for low <= high {
+		var mid int = low + ((high - low) >> 1)
+		if array[mid] > value {
+			high = mid - 1
+		} else if array[mid] < value {
+			low = mid + 1
+		} else {
+			if mid == length-1 {
+				return mid
+			} else if array[mid+1] != value {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		}
+	}
+	return -1
+}
+
+// 查找第一个大于等于给定值的元素
+func BinarySearchFirstGE(array []int, value int) int {
+	var length int = len(array)
+	if length == 0 {
+		return -1
+	}
+	var low int = 0
+	var high int = length - 1
+	for low <= high {
+		var mid int = low + ((high - low) >> 1)
+		if array[mid] >= value {
+			if mid == 0 {
+				return mid
+			} else if array[mid-1] < value {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
+
+// 查找最后一个小于等于给定值的元素
+func BinarySearchLastLE(array []int, value int) int {
+	var length int = len(array)
+	if length == 0 {
+		return -1
+	}
+	var low int = 0
+	var high int = length - 1
+	for low <= high {
+		var mid int = low + ((high - low) >> 1)
+		if array[mid] <= value {
+			if mid == length-1 {
+				return mid
+			} else if array[mid+1] > value {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
--- binarysearch_test.go
+++ binarysearch_test.go
@@ -1,12 +1,10 @@
 package algorithm
 
 import (
-	"math"
 	"testing"
 )
 
 func TestBinarySearch(t *testing.T) {
-	math.Sqrt(2)
 	var (
 		array    = []int{1, 3, 5, 7, 10, 14}
 		expected = 4
@@ -17,5 +15,75 @@ func TestBinarySearch(t *testing.T) {
 		t.Logf("二分查找：the index of %d in %v is %d", input, array, actual)
 	} else {
 		t.Errorf("二分查找：the index of %d in %v is %d; expected %v", input, array, actual, expected)
+	}
+}
+
+func TestBinarySearchForRotateArray(t *testing.T) {
+	var (
+		array    = []int{5, 6, 7, 0, 1, 2, 3, 4}
+		expected = 2
+	)
+	var input int = 7
+	actual := BinarySearchForRotateArray(array, input)
+	if actual == expected {
+		t.Logf("二分查找：the index of %d in %v is %d", input, array, actual)
+	} else {
+		t.Errorf("二分查找：the index of %d in %v is %d; expected %v", input, array, actual, expected)
+	}
+}
+
+func TestBinarySearchFirst(t *testing.T) {
+	var (
+		array    = []int{1, 3, 5, 5, 7, 14}
+		expected = 2
+	)
+	var input int = 5
+	actual := BinarySearchFirst(array, input)
+	if actual == expected {
+		t.Logf("二分查找：the first index of %d in %v is %d", input, array, actual)
+	} else {
+		t.Errorf("二分查找：the first index of %d in %v is %d; expected %v", input, array, actual, expected)
+	}
+}
+
+func TestBinarySearchLast(t *testing.T) {
+	var (
+		array    = []int{1, 3, 5, 5, 7, 14}
+		expected = 3
+	)
+	var input int = 5
+	actual := BinarySearchLast(array, input)
+	if actual == expected {
+		t.Logf("二分查找：the last index of %d in %v is %d", input, array, actual)
+	} else {
+		t.Errorf("二分查找：the last index of %d in %v is %d; expected %v", input, array, actual, expected)
+	}
+}
+
+func TestBinarySearchFirstGE(t *testing.T) {
+	var (
+		array    = []int{1, 3, 5, 5, 7, 14}
+		expected = 4
+	)
+	var input int = 6
+	actual := BinarySearchFirstGE(array, input)
+	if actual == expected {
+		t.Logf("二分查找：the index of first element greater than or equal to %d in %v is %d", input, array, actual)
+	} else {
+		t.Errorf("二分查找：the index of first element greater than or equal to %d in %v is %d; expected %v", input, array, actual, expected)
+	}
+}
+
+func TestBinarySearchLastLE(t *testing.T) {
+	var (
+		array    = []int{1, 3, 5, 5, 7, 14}
+		expected = 3
+	)
+	var input int = 6
+	actual := BinarySearchLastLE(array, input)
+	if actual == expected {
+		t.Logf("二分查找：the index of last element less than or equal to %d in %v is %d", input, array, actual)
+	} else {
+		t.Errorf("二分查找：the index of last element less than or equal to %d in %v is %d; expected %v", input, array, actual, expected)
 	}
 }
