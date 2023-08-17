b6b5dc396e583598e251807291113805eedf99e1
loong917 <loong.me@outlook.com>
2023-08-17 17:32:30 +0800

    add 二叉树

--- binarytree.go
+++ binarytree.go
@@ -68,8 +68,8 @@ func (tree *BinaryTree) Delete(data int) {
 		return
 	}
 	// 要删除的节点有2个子节点
-	// 首先查找删除节点的后继节点，即删除节点右子树中最小的节点；
-	// 然后将删除节点后继节点的值覆盖删除节点，并将后继节点的右子树挂载到后继节点的父节点上；
+	//
+	// 首先查找删除节点的后继节点，即删除节点右子树中最小的节点；然后将删除节点后继节点的值覆盖删除节点，并将后继节点的右子树挂载到后继节点的父节点上；
 	if current.left != nil && current.right != nil {
 		// 查找右子树中最小的节点
 		minParent := current
@@ -84,8 +84,11 @@ func (tree *BinaryTree) Delete(data int) {
 		parent = minParent
 	}
 	// 要删除的节点是叶子节点或者只有1个子节点
+	//
 	// 情况1：如果删除节点是叶子节点，只需删除引用
+	//
 	// 情况2：如果删除节点只存在左子节点，将删除节点的左子树挂载到父节点上
+	//
 	// 情况3：如果删除节点只存在右子节点，将删除节点的右子树挂载到父节点上
 	var child *TreeNode
 	if current.left != nil {
@@ -97,7 +100,7 @@ func (tree *BinaryTree) Delete(data int) {
 	}
 
 	if parent == nil {
-		tree.root = nil
+		tree.root = child
 	} else if parent.left == current {
 		parent.left = child
 	} else if parent.right == current {
@@ -106,7 +109,8 @@ func (tree *BinaryTree) Delete(data int) {
 }
 
 // 前序遍历
-// 前序遍历是指对于树中的任意节点来说，先打印这个节点，然后再打印它的左子树，最后打印它的右子树
+//
+// 对于树中的任意节点来说，先打印这个节点，然后再打印它的左子树，最后打印它的右子树
 func PreOrderLoop(node *TreeNode) []int {
 	if node == nil {
 		return nil
@@ -130,7 +134,8 @@ func PreOrderLoop(node *TreeNode) []int {
 }
 
 // 中序遍历
-// 中序遍历是指对于树中的任意节点来说，先打印它的左子树，然后再打印它本身，最后打印它的右子树
+//
+// 对于树中的任意节点来说，先打印它的左子树，然后再打印它本身，最后打印它的右子树
 func InOrderLoop(node *TreeNode) []int {
 	if node == nil {
 		return nil
@@ -145,7 +150,8 @@ func InOrderLoop(node *TreeNode) []int {
 }
 
 // 后序遍历
-// 后序遍历是指对于树中的任意节点来说，先打印它的左子树，然后再打印它的右子树，最后打印这个节点本身
+//
+// 对于树中的任意节点来说，先打印它的左子树，然后再打印它的右子树，最后打印这个节点本身
 func PostOrderLoop(node *TreeNode) []int {
 	if node == nil {
 		return nil
